ab34384a35a008f50dfcb16ac3e79d6699c7b5ad
njoki <njerinjoki22@gmail.com>
2025-12-02 13:50:32 +0300

    edit about copy

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,31 @@
+import Logo from "../components/Logo";
+import SocialLinks from "../components/SocialLinks";
+import {ArrowUpRight} from "lucide-react";
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <article className="min-h-screen bg-zinc-950 text-white">
+      <nav className="flex gap-4 justify-between px-6 py-12">
+        <Logo /> 
+        <SocialLinks 
+          className="px-10 py-14 text-stone-500"
+        />
+      </nav>
+      <div className="flex-1 px-8 py-4 w-full lg:w-5/6 flex flex-col gap-8 font-inter text-xl leading-9  text-stone-400">
+        <p>
+            <a 
+              href="http://" /** add home url */
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-3 font-climate text-6xl "
+          >
+          UI <sub className="text-2xl">playground</sub>
+        </a> {" "} is a space where I showcase design implementations built while learning modern web frameworks. 
+                  Each recreation emphasizes accurate styling, performant interactions, and accessible markup
+        </p>
+      </div>
+    </article>
   )
 }
 
