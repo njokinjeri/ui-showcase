84eff8c2c3453e94c4ddb5f5568c95b3e8aed1c9
njoki <njerinjoki22@gmail.com>
2026-01-14 15:15:56 +0300

    fix nav spacing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,14 @@ import SocialLinks from "./SocialLinks";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center p-6 mt-6">
+    <nav className="w-screen flex justify-between items-center p-6 mt-6">
         <Logo />
-        <div className="flex space-evenly gap-8 px-6 text-stone-400">
+        <div className="flex space-evenly gap-6 px-6 text-stone-400">
           <NavLink  
             to="/about"
             className="font-montserrat text-xl hover:text-stone-100 hover:underline hover:underline-offset-8 hover:decoration-violet-200"
-            >About</NavLink>
+            >About
+          </NavLink>
           <SocialLinks className="place-items-center"/>
         </div>
     </nav>
